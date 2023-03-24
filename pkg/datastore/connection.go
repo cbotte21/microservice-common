5c54f64dadc54f194c436cbc1b18fcac39184052
Cody Botte <cbotte21@gmail.com>
2023-03-24 17:32:37 -0400

    made database a singleton object

--- pkg/datastore/connection.go
+++ pkg/datastore/connection.go
@@ -8,21 +8,23 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+var mongoClient *mongo.Client = nil
+
 func GetMongoClient() (*mongo.Client, error) {
-	//connect
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(enviroment.GetEnvVariable("mongo_uri")))
+	if mongoClient == nil {
+		//connect
+		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(enviroment.GetEnvVariable("mongo_uri")))
 
-	//error check
-	if err != nil {
-		return nil, err
+		//error check
+		if err != nil {
+			return nil, err
+		}
+
+		//ping
+		if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+			panic(err)
+			return nil, err
+		}
 	}
-
-	//ping
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
-		return nil, err
-	}
-
-	//return
-	return client, nil
+	return mongoClient, nil
 }
