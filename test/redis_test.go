033db7e9527d1c21d309ac0eea4fdeaaeaeb037d
Samkist <samshark23451@gmail.com>
2023-05-16 19:43:46 -0400

    rewrote tests

--- test/redis_test.go
+++ test/redis_test.go
@@ -1,93 +1,80 @@
 package test
 
 import (
-	"context"
 	"github.com/cbotte21/microservice-common/pkg/datastore"
 	"github.com/cbotte21/microservice-common/pkg/schema"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestPubSubRedis(t *testing.T) {
 	client := datastore.RedisClient[schema.User]{}
 
-	err := client.Init()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	mock := client.MockInit()
+	mock.MatchExpectationsInOrder(false)
+	_ = client.Publish("test", "hello")
+	_ = client.Publish("test2", "hello")
 
-	sub := client.Subscribe("test", "test2")
-	pub := client.Publish("test", "hello")
-	pub2 := client.Publish("test2", "hello")
-
-	if sub == nil || pub != nil || pub2 != nil {
-		if sub == nil {
-			t.Fatalf("Subscription was null")
-		}
-
-		if pub != nil {
-			t.Fatalf(pub.Error())
-		}
-
-		t.Fatalf(pub2.Error())
-	}
-
-	msgCount := 0
-
-	for {
-		_, err := sub.ReceiveMessage(context.Background())
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
-
-		msgCount++
-
-		if msgCount == 2 {
-			break
-		}
-	}
-
-	if msgCount != 2 {
-		t.Fatalf("Message count was incorrect")
-	}
+	mock.ExpectPublish("test", "hello")
+	mock.ExpectPublish("test2", "hello")
 }
 
-func TestCreateRedis(t *testing.T) {
+func TestMockCreateFind(t *testing.T) {
 	client := datastore.RedisClient[schema.User]{}
 
 	user1 := schema.User{Id: "1", Email: "john@redis.com", Password: "Asdfasdf1"}
 	user2 := schema.User{Id: "2", Email: "cbotte21@gmail.com", Password: "Asdfasdf1"}
 
-	err := client.Init()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	mock := client.MockInit()
+	mock.MatchExpectationsInOrder(false)
+	mock.ExpectSet(user1.Id, user1, 10*time.Second).SetVal("OK")
+	mock.ExpectSet(user2.Id, user2, 10*time.Second).SetVal("OK")
+	mock.ExpectGet(user1.Id).SetVal(Marshal(t, user1))
+	mock.ExpectGet(user2.Id).SetVal(Marshal(t, user2))
 
-	err = client.Create(user1)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	err := client.Create(user1)
+	assert.NoError(t, err)
 	err = client.Create(user2)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	assert.NoError(t, err)
+
+	_, err = client.Find(schema.User{Id: "1"})
+	assert.NoError(t, err)
+	_, err = client.Find(schema.User{Id: "2"})
+	assert.NoError(t, err)
 }
 
-func TestFindRedis(t *testing.T) {
-	client := datastore.RedisClient[schema.User]{}
+func TestPubSub(t *testing.T) {
+	client, err := GetRedis()
+	assert.NoError(t, err)
+	ps := client.Subscribe("test")
+	go func() {
+		for msg := range ps.Channel() {
+			if len(msg.Payload) > 0 {
+				break
+			}
+		}
+	}()
+	err = client.Publish("test", "test")
+	assert.NoError(t, err)
+}
 
-	user := schema.User{Id: "1", Email: "cody@redis.com", Password: "Asdfasdf1"}
+func TestCreateFind(t *testing.T) {
+	client, err := GetRedis()
+	assert.NoError(t, err)
 
-	err := client.Init()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	user1 := schema.User{Id: "1", Email: "john@redis.com", Password: "Asdfasdf1"}
+	user2 := schema.User{Id: "2", Email: "cbotte21@gmail.com", Password: "Asdfasdf1"}
 
-	res, err := client.Find(user)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+	err = client.Create(user1)
+	assert.NoError(t, err)
+	err = client.Create(user2)
+	assert.NoError(t, err)
 
-	if res.Email != user.Email {
-		t.Fatalf("received incorrect data!")
-	}
+	res, err := client.Find(user1)
+	assert.NoError(t, err)
+	assert.Equal(t, user1, res)
+	res, err = client.Find(user2)
+	assert.NoError(t, err)
+	assert.Equal(t, user2, res)
 }
