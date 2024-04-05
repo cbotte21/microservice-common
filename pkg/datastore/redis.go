cf46d8606ca21b173991167f6c3a1f24a5dd67ae
Cody Botte <cbotte21@gmail.com>
2024-04-05 16:04:45 -0400

    debug

--- pkg/datastore/redis.go
+++ pkg/datastore/redis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/cbotte21/microservice-common/pkg/environment"
 	"github.com/cbotte21/microservice-common/pkg/schema"
 	"github.com/go-redis/redismock/v9"
@@ -83,8 +84,11 @@ func (client *RedisClient[T]) Dequeue() (T, error) {
 	if len(resInArr) == 0 {
 		return client.Schema, errors.New("queue is empty")
 	}
+	fmt.Println(resInArr)
 	res := resInArr[0]
+	fmt.Println(res)
 	var parsed T
 	err = json.Unmarshal([]byte(res), &parsed)
+	fmt.Println(parsed)
 	return parsed, err
 }
